748f1e4b1e206d1d43178a3b0b2c22704c16d678
cassandra <kasandra999k@gmail.com>
2025-05-13 19:51:27 +0900

    commit

--- app/config.py
+++ app/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     spotipy_client_id: str
     spotipy_client_secret: str
     spotipy_redirect_uri: str  # 사실상 사용하지 않는 필드
+    database_url: str
 
     class Config:
         env_file = ".env"
